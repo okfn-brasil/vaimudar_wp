f1d14812cbea7517c4ef00752441eaee2cbcf3bd
root <admin@vaimudar.org>
2014-05-19 01:49:30 -0400

    Changing password for database access

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'proprietarios');
+define('DB_NAME', 'proprietariosdobrasil_wp');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'proprietarios_wp');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '.cachorrosemproprietariomorredefome.');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
